b66c1deacc62af7e1949580d96fc303ec131ed56
Karthikeya-24k <karthikeyakmshastri@gmail.com>
2024-08-15 22:25:38 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,12 @@
+- 👋 Hi, I’m @Karthikeya-24k
+- 👀 I’m interested in backend devolupment
+- 🌱 I’m currently learning BCA
+- 💞️ I’m looking to collaborate on Java
+- 📫 How to reach me karthikeyakmshasrtri@gmail.com
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+
+<!---
+Karthikeya-24k/Karthikeya-24k is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
